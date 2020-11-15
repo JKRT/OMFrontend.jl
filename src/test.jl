be0b7a1b51b8f0ff7bba43b92fcc887149d2e4c9
John Tinnerholm <jtinnerholm@gmail.com>
2020-11-15 18:03:58 +0100

    Minor compilation changes

--- src/test.jl
+++ src/test.jl
@@ -2,8 +2,8 @@ import HybridDAEParser
 #using BenchmarkTools
 p = HybridDAEParser.parseFile("example.mo")
 scodeProgram = HybridDAEParser.translateToSCode(p)
-@info "Translation to SCode"
-@info "SCode -> DAE"
+@debug "Translation to SCode"
+@debug "SCode -> DAE"
 (dae, cache) = HybridDAEParser.instantiateSCodeToDAE("HelloWorld", scodeProgram)
-@info "After DAE Translation"
+@debug "After DAE Translation"
 @show dae

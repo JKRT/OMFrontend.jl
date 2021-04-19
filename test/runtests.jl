6d0e0592cc6804d38ff0703ca995ecf180066412
John Tinnerholm <jtinnerholm@gmail.com>
2021-04-19 18:53:37 +0200

    Added some initial testfiles for frontend tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,108 @@
+#=
+  Author: John Tinnerholm johti17@liu.se
+
+Current Status:
+
+Works when including the file in the terminal. Does not seem to work when run via tests.
+
+=#
+
+import HybridDAEParser
+import Absyn
+import SCode
+import DAE
+
 using Test
-@test 1 == 1
+
+function flatten(modelName::String, modelFile::String)
+  p = HybridDAEParser.parseFile(modelFile)
+  scodeProgram = HybridDAEParser.translateToSCode(p)
+  (dae, cache) = HybridDAEParser.instantiateSCodeToDAE(modelName, scodeProgram)
+end
+
+function parseAndLowerToScode(modelName::String, modelFile::String)
+  p = HybridDAEParser.parseFile(modelFile)
+  scodeProgram = HybridDAEParser.translateToSCode(p)
+end
+
+@testset "Absyn -> SCode test" begin
+  @test try
+    parseAndLowerToScode("HelloWorld", "./Models/HelloWorld.mo")
+    true
+  catch e
+    throw(e)
+    false
+  end
+  @test try
+    parseAndLowerToScode("LotkaVolterra", "./Models/LotkaVolterra.mo")
+    true
+  catch e
+    throw(e)
+    false
+  end
+  @test try
+    parseAndLowerToScode("Robertsson", "./Models/Robertsson.mo")
+    true
+  catch e
+    throw(e)
+    false
+  end
+  @test try
+    parseAndLowerToScode("SimpleCircuit", "./Models/SimpleCircuit.mo")
+    true
+  catch e
+    throw(e)
+    false
+  end
+  @test try
+    parseAndLowerToScode("SimpleMechanicalSystem", "./Models/SimpleMechanicalSystem.mo")
+    true
+  catch e
+    throw(e)
+    false
+  end  
+end
+
+@testset "SCode -> DAE test" begin
+
+  @test try
+    flatten("HelloWorld", "./Models/HelloWorld.mo")
+    true
+  catch e
+    throw(e)
+    false
+  end
+
+  @test try
+    flatten("LotkaVolterra", "./Models/LotkaVolterra.mo")
+    true
+  catch e
+    throw(e)
+    false
+  end
+
+  @test try
+    flatten("Robertsson", "./Models/Robertsson.mo")
+    true
+  catch e
+    throw(e)
+    false
+  end
+  
+  @test try
+    flatten("SimpleCircuit", "./Models/SimpleCircuit.mo")
+    true
+  catch e
+    throw(e)
+    false
+  end
+
+  @test try
+    flatten("SimpleMechanicalSystem", "./Models/SimpleMechanicalSystem.mo")
+    true
+  catch e
+    throw(e)
+    false
+  end
+  
+end

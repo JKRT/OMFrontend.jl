87726ec35b64b6e8189fc03ffec785c145b00deb
John Tinnerholm <jtinnerholm@gmail.com>
2021-04-22 19:59:56 +0200

    Updated ImmutableList in the build

--- deps/build.jl
+++ deps/build.jl
@@ -2,3 +2,5 @@
 import Pkg; Pkg.add("Pkg")
 Pkg.add("OpenModelicaParser")
 Pkg.build("OpenModelicaParser")
+#= We use master directly..=#
+Pkg.add(url="https://github.com/OpenModelica/ImmutableList.jl.git")

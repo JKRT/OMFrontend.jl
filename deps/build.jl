48ec7a3305dc14d954d4caadb8c1ce4e4c336bea
John Tinnerholm <jtinnerholm@gmail.com>
2021-12-05 16:54:28 +0100

    Removed the old parser as a dependency

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,6 @@
 @info "OMFrontend Starting build script"
 import Pkg; Pkg.add("Pkg")
-Pkg.add("OpenModelicaParser")
-Pkg.build("OpenModelicaParser")
+Pkg.add("OMParser")
+Pkg.build("OMParser")
 #= We use master directly..=#
 Pkg.add(url="https://github.com/OpenModelica/ImmutableList.jl.git")
